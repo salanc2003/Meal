c515fc3becda5ad44678ef9c7e51de1079b5e6bb
salanc2003 <salanc.cheng@gmail.com>
2015-06-27 12:52:13 +0800

    @Second update

--- MealOrder.sql
+++ MealOrder.sql
@@ -4,21 +4,32 @@ CREATE DATABASE MealOrder CHARACTER SET big5;
 
 USE MealOrder;
 
+DROP TABLE IF EXISTS `MealKind`;
+
+CREATE TABLE `MealKind` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` char(35) NOT NULL DEFAULT '',
+  `RestaurantID` char(35) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`)
+);
+
 DROP TABLE IF EXISTS `Meal`;
 
 CREATE TABLE `Meal` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` char(35) NOT NULL DEFAULT '',
   `Description` char(35) NOT NULL DEFAULT '',
-  `MealKindID` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`)
+  `MealKindID` int(11) NOT NULL DEFAULT 0,
+  `Price` int(11) NOT NULL DEFAULT 99999,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (MealKindID) REFERENCES MealKind (ID)
 );
 
 DROP TABLE IF EXISTS `OrderMeal`;
 
 CREATE TABLE `OrderMeal`(
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `RestaurantID` int(11) NOT NULL DEFAULT '',
+  `RestaurantID` int(11) NOT NULL DEFAULT 0,
   `Kind` ENUM('Here', 'ToGo', 'CallOut') NOT NULL,
   `TableNum` SMALLINT(5) UNSIGNED NOT NULL,
   `CustomID` int(11) DEFAULT 0,
@@ -35,16 +46,16 @@ CREATE TABLE `OrderMealDetail`(
   `ID` int(11),
   `MealID` int(11) NOT NULL,
   `Amount` SMALLINT(5) NOT NULL DEFAULT 1,
-  `ExtraID`int(11) DEFAULT 0,
+  `ExtraMealID`int(11) DEFAULT 0,
   PRIMARY KEY (`ID`)
 );
 
-DROP TABLE IF EXISTS `MealKind`;
+DROP TABLE IF EXISTS `ExtraMeal`;
 
-CREATE TABLE `MealKind` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` char(35) NOT NULL DEFAULT '',
-  `RestaurantID` char(35) NOT NULL DEFAULT '',
+CREATE TABLE `ExtraMeal`(
+  `ID` int(11),
+  `MealID` int(11) NOT NULL,
+  `Price`int(11) DEFAULT 99999,
   PRIMARY KEY (`ID`)
 );
 
